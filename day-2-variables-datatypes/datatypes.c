9a94c2418f97e7fac00352647088d1c09283812b
navanitha-csbs <eswaranavanitha@gmail.com>
2026-05-11 12:48:32 +0530

    Added Datatype program

---
+++ day-2-variables-datatypes/datatypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 100;
+    float b = 3.14;
+    char c = 'X';
+
+    printf("%d\n", a);
+    printf("%.2f\n", b);
+    printf("%c\n", c);
+
+    return 0;
+}
