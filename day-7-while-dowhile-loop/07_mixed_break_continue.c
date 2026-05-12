3df18ed07abfa42d0eb9c38bfc02158f5fadc64c
navanitha-csbs <eswaranavanitha@gmail.com>
2026-05-12 17:19:30 +0530

    Added mixed break and continue logic program

---
+++ day-7-while-dowhile-loop/07_mixed_break_continue.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+
+    int i;
+
+    for(i=1; i<=10; i++)
+    {
+        if(i == 8)
+        {
+            break;
+        }
+
+        if(i == 5)
+        {
+            continue;
+        }
+
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
